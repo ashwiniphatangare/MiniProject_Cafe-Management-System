492f1dfe1078d057b1b2b6ab910eb8c9a2902556
ashwiniphatangare <108507537+ashwiniphatangare@users.noreply.github.com>
2022-08-16 13:37:12 +0530

    Add files via upload

---
+++ MiniProject_Cafe-Management-System/src/MiniProject/cafe.java
@@ -0,0 +1,25 @@
+package MiniProject;
+
+
+
+public class cafe {
+
+	 String ItemName;
+	double price;
+	public String getItemName() {
+		return ItemName;
+	}
+	public void setItemName(String itemName) {
+		ItemName = itemName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public void info() {
+		System.out.println(ItemName + "\t" + price);
+	}
+
+}
